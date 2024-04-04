af17fe5e56fedf3f66546936248fbc1fdc91da49
bhkh007 <khandarbhavesh007@gmail.com>
2024-04-04 17:45:53 +0530

    Commit these files to my CDAC_DSA repository and push

---
+++ CDAC_ADS/src/Moodle/GCD.java
@@ -0,0 +1,30 @@
+package Moodle;
+
+import java.util.Scanner;
+
+public class GCD {
+
+	public static int gcd (int a, int b) {
+		if (b==0) {
+		return a;
+		}else {
+			return gcd(b,a % b);
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter first number : ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter second number : ");
+		int num2 = sc.nextInt();
+		
+		int result =gcd(num1, num2);
+		System.out.println("Gcd of " +num1 + " and " + num2 +" = "+result);
+		sc.close();
+		
+	}
+
+}
---
+++ CDAC_ADS/src/Moodle/LCM.java
@@ -0,0 +1,30 @@
+package Moodle;
+import java.util.Scanner;
+
+public class LCM {
+	
+	public static int lcm(int num1,int num2) {
+		return (num1*num2) / gcd (num1,num2);
+	}
+	
+	public static int gcd(int num1,int num2) {
+		if(num2==0) {
+			return num1;
+		}else {
+			return gcd(num2, num1 %num2);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter num1 : ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter num1 : ");
+		int num2 = sc.nextInt();
+		
+		
+		System.out.println("LCM of" + num1 +" and "+ num2+ " = " + LCM.lcm(num1, num2) );
+		
+		sc.close();
+	}
+}
