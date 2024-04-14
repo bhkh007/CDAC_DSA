8e4976b07db01bc7e4db0bdad525d761e79a29ff
bhkh007 <khandarbhavesh007@gmail.com>
2024-04-14 10:53:35 +0530

    Commit changes

--- CDAC_ADS/src/assignment3/MiddleNode.java
+++ CDAC_ADS/src/assignment3/MiddleNode.java
@@ -27,12 +27,16 @@ public class MiddleNode {
 		return slow;
 	}
 	
-	public static void main(String...args) {
+	public static void main(String[]args) {
 		ListNode head = new ListNode(1);
 		head.next = new ListNode(2);
 		head.next.next = new ListNode(3);
 		head.next.next.next = new ListNode(4);
 		head.next.next.next.next = new ListNode(5);
+		
+		ListNode middle = findMiddle(head);
+		
+		System.out.println("Middle element is : " + middle.val);
 		
 	}
 	
