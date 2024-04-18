17e8b16fcad8b38e70e6ffb3176f08ad73ffc2a7
bhkh007 <khandarbhavesh007@gmail.com>
2024-04-19 00:27:58 +0530

    Commit this file for my repository

---
+++ CDAC_ADS/src/assignment5/MergeSort.java
@@ -0,0 +1,67 @@
+package assignment5;
+
+public class MergeSort {
+
+	public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        int[] temp = new int[arr.length];
+        mergeSort(arr, temp, 0, arr.length - 1);
+    }
+
+    private static void mergeSort(int[] arr, int[] temp, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, temp, left, mid);
+            mergeSort(arr, temp, mid + 1, right);
+            merge(arr, temp, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int[] temp, int left, int mid, int right) {
+    	for (int i = left; i <= right; i++) {
+            temp[i] = arr[i];
+        }
+
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (temp[i] <= temp[j]) {
+                arr[k] = temp[i];
+                i++;
+            } else {
+                arr[k] = temp[j];
+                j++;
+            }
+            k++;
+        }
+       
+        while (i <= mid) {
+            arr[k] = temp[i];
+            k++;
+            i++;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        mergeSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
