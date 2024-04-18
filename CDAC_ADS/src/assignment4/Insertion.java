6f12584a62544fe374db9824681bd07fc67a0b94
bhkh007 <khandarbhavesh007@gmail.com>
2024-04-18 22:31:04 +0530

    Commit

--- CDAC_ADS/src/assignment4/Insertion.java
+++ CDAC_ADS/src/assignment4/Insertion.java
@@ -1,5 +1,73 @@
 package assignment4;
 
 public class Insertion {
+    
+    static class Node {
+        int data;
+        Node prev;
+        Node next;
 
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    public Insertion() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void displayForward() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public void displayBackward() {
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.prev;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Insertion dll = new Insertion();
+        dll.insertAtEnd(10);
+        dll.insertAtEnd(20);
+        dll.insertAtBeginning(5);
+        dll.displayForward(); // Output: 5 10 20
+        dll.displayBackward(); // Output: 20 10 5
+    }
 }
